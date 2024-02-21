bc229ba596e82d3f3548d4f423f2c8dc89ee67d0
owenery <126583938+owenery@users.noreply.github.com>
2024-02-21 11:53:33 +0700

    Update MTitik.java

--- Praktikum PBO 1/MTitik.java
+++ Praktikum PBO 1/MTitik.java
@@ -2,11 +2,11 @@ public class MTitik {
 	public static void main(String[] args) {
 		Titik t1;
 		Titik t2;
-        Titik t3;
+        	Titik t3;
 
 		t1 = new Titik();
 		t2 = new Titik();
-        t3 = new Titik(5,6);
+        	t3 = new Titik(5,6);
 
 		t1.setAbsis(5);
 		t1.setOrdinat(7);
@@ -20,19 +20,19 @@ public class MTitik {
 		counterTitik = Titik.getCounterTitik();
 		System.out.println("Jumlah objek titik: " + counterTitik);
         
-        absis = t1.getAbsis();
-        ordinat = t1.getOrdinat();
-        System.out.println("t1("+absis + ", " + ordinat + ")");
-        System.out.printf("t1(%.1f, %.1f)\n", absis, ordinat);
-
-        absis = t2.getAbsis();
+	        absis = t1.getAbsis();
+	        ordinat = t1.getOrdinat();
+	        System.out.println("t1("+absis + ", " + ordinat + ")");
+	        System.out.printf("t1(%.1f, %.1f)\n", absis, ordinat);
+	
+	        absis = t2.getAbsis();
 		ordinat = t2.getOrdinat();
 		System.out.printf("t1(%.1f, %.1f)\n", absis, ordinat);
 		System.out.println("t2("+absis + ", " + ordinat + ")");
-
-        absis = t3.getAbsis();
-        ordinat = t3.getOrdinat();
-        System.out.printf("t3(%.1f, %.1f)\n", absis, ordinat);
-        System.out.println("t3("+ absis + ", " + ordinat + ")");
+	
+	        absis = t3.getAbsis();
+	        ordinat = t3.getOrdinat();
+	        System.out.printf("t3(%.1f, %.1f)\n", absis, ordinat);
+	        System.out.println("t3("+ absis + ", " + ordinat + ")");
 	}
 }
