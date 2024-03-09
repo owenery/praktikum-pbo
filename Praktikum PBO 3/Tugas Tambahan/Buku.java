cc9a54bc186d6508ba601dab6b176ce0201e2575
David Nugroho <126583938+owenery@users.noreply.github.com>
2024-03-09 21:29:28 +0700

    Add files via upload

---
+++ Praktikum PBO 3/Tugas Tambahan/Buku.java
@@ -0,0 +1,28 @@
+public class Buku {
+    //atribut
+    private String judul;
+    private boolean tersedia;
+
+    //method
+    public Buku(String judul) {
+        this.judul = judul;
+        tersedia = true;
+    }
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public boolean isTersedia() {
+        return tersedia;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+
+    public void setTersedia(boolean tersedia) {
+        this.tersedia = tersedia;
+    }
+}
+    
